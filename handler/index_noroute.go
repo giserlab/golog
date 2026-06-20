71114cba64f638b87e4cc301f4c3e5baf0cd8375
WShihan <3443327820@qq.com>
2026-06-20 14:47:31 +0800

    refactor(web): 优化404页面

--- handler/index_noroute.go
+++ handler/index_noroute.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net/http"
 
+	"golog/entity"
 	"golog/store"
 	"golog/system"
 
@@ -23,6 +24,16 @@ func noRoute(c *gin.Context) {
 		c.AbortWithStatus(http.StatusNotFound)
 		return
 	}
+	var routes = []entity.Route{
+		{
+			Name: "首页",
+			Path: "/",
+		},
+		{
+			Name: "404",
+			Path: "",
+		},
+	}
 	navs, err := store.ListNavigations()
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
@@ -30,6 +41,7 @@ func noRoute(c *gin.Context) {
 	}
 	var tpl bytes.Buffer
 	if err := system.NotFoundTmpl.Execute(&tpl, data(c, gin.H{
+		"Routes":      routes,
 		"Navigations": navs,
 	})); err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
