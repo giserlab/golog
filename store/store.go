d78fa94252243a546976db9bde1c3d05a0e524fd
WShihan <3443327820@qq.com>
2026-07-07 02:15:58 +0800

    chore: 数据库连接新增wal参数

--- store/store.go
+++ store/store.go
@@ -13,7 +13,7 @@ var db *sql.DB
 
 func init() {
 	var err error
-	db, err = sql.Open("sqlite", "file:db.sqlite?cache=shared")
+	db, err = sql.Open("sqlite", "file:db.sqlite?cache=shared&_journal_mode=WAL&_synchronous=NORMAL&_busy_timeout=5000")
 	if err != nil {
 		log.Fatalln(err)
 	}
